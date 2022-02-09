1c49302e5e948762857b36f002a54c4db22f4019
Andrew Whigham <awhigham9@gmail.com>
2022-02-09 15:08:33 -0500

    Started polling cmds - WIP

---
+++ src/commands/poll.ts
@@ -0,0 +1,22 @@
+import { BaseCommandInteraction, Constants } from "discord.js";
+import { ICommand } from "../icommand";
+
+export const startPoll: ICommand = {
+    name: "startPoll",
+    description: "Opens a poll",
+    options: [
+        {
+            name: "pollname",
+            description: "The name of the poll",
+            required: true,
+            type: Constants.ApplicationCommandOptionTypes.STRING
+        }
+    ],
+    execute: async (interaction: BaseCommandInteraction) => {
+        await interaction.reply('NOT IMPELEMTED: Tried to create poll with name ');
+    }
+}
+
+export function getCommands(): ICommand[] {
+    return [startPoll];
+}
