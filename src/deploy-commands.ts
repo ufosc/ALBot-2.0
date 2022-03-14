7cfb3184bd9bb3f394eec4a8241fdda64674ca55
Santosh <stirumala@ufl.edu>
2022-03-14 17:45:54 -0400

    some testing

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -9,7 +9,7 @@ import logger from './logging/logging';
 const rest = new REST({ version: '9' }).setToken(token);
 
 const commandList: ICommand[] = [];
-logger.error("test5")
+logger.error("test15")
 for (const commandName in commands) {
 	commandList.push(commands[commandName]);
 }
--- src/logging/logging.ts
+++ src/logging/logging.ts
@@ -17,7 +17,7 @@ const logger: Logger = winston.createLogger({
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
     new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: './logs/combined.log', level: 'info' }),
+    new winston.transports.File({ filename: './logs/combined.log' }),
   ],
 
 }) 
