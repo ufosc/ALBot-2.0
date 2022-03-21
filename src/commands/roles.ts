28e42cd3c90515f77b3cfa3225f1dbb8552c8061
Santosh <stirumala@ufl.edu>
2022-03-21 17:53:03 -0400

    add roles command

---
+++ src/commands/roles.ts
@@ -0,0 +1,42 @@
+import { Base, BaseCommandInteraction, Constants, Interaction} from "discord.js";
+import { ICommand } from "../icommand";
+import config from '../../config.json'
+let directions = new Set<string>();
+directions.add("club")
+class Role {
+
+
+}
+
+const GiveRole: ICommand = {
+    name: "giverole",
+    description: "Gives a user a role",
+    options: [
+        {
+            name: "role",
+            description: "Role that user wants",
+            required: true,
+            type: Constants.ApplicationCommandOptionTypes.STRING
+        }
+    ],
+    execute: async (interaction: BaseCommandInteraction) => {
+        let name = interaction.options.get("role")?.value;
+        if(directions.has(name as string))
+		{
+            var guild: any = interaction.client.guilds.cache.get(config.guildId)
+            const member = await guild.members.fetch(interaction.user.id)
+            const role = await guild.roles.cache.find((r: any) => r.name === 'club')
+            member.roles.add(role)
+            await interaction.reply(`Added to role successfully!`);
+
+        }
+        else 
+        {
+            await interaction.reply(`No such role`);
+
+        }
+    }
+}
+export function getCommands(): ICommand[] {
+    return [GiveRole];
+}
