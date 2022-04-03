f11960596776fdc57ff7c198f134343758426d30
Dev Patel <devpatel2556@gmail.com>
2022-04-03 10:46:51 -0400

    embeds are editable through file

--- src/commands/projects.ts
+++ src/commands/projects.ts
@@ -1,6 +1,7 @@
 import { BaseCommandInteraction, MessageActionRow, MessageButton, MessageComponentInteraction, MessageEmbed} from "discord.js";
 import { ICommand } from "../icommand";
 import config from '../../config.json'
+import projectData from "../projects.json"
 
 export const projects: ICommand = {
     name: "projects",
@@ -40,46 +41,29 @@ export const projects: ICommand = {
         const remove = new MessageActionRow().addComponents([backward, forward, removeRole])
         var row = assign
 
-        // Embed and Embed Resources
-        var Albot_resources = `
-                        [Intro to TypeScript](https://youtu.be/gp5H0Vw39yw)
-                        [Discord.js Documentation](https://discord.js.org/#/)
-                        [Discrod.js Guide](https://discordjs.guide/#before-you-begin)
-                        `
-
-        var Albot = new MessageEmbed()
-            .setTitle("ALBot-2.0")
-            .setDescription(`Alpha version of ALBot 2.0, the spiritual successor to ALBot
-                            <:octocat:830153162735747152> [Github Repo](https://github.com/ufosc/ALBot-2.0)
-                            :star: 4`)
-            .setColor("RED")
-            .addField("Language:", "Typescript", true)
-            .addField("Roles:", "bot-dev", true)
-            .addField("Resources:", Albot_resources, false)
-        
-        var Documiner = new MessageEmbed()
-            .setTitle("DocuMiner")
-            .setDescription(`A production-ready pipeline for text mining and subject indexing
-                            <:octocat:830153162735747152> [Github Repo](https://github.com/ufosc/DocuMiner)
-                            :star: 11`)
-            .setColor("RED")
-            .addField("Language:", "Python", true)
-            .addField("Roles:", "Documiner", true)
-            .addField("Resources", "None", false)
-            
         var page = 0
-        var projects = [Albot, Documiner]
+        var keys = Object.keys(projectData)
+
+        const getProject = (page: number) => {
+            // Get project key based on page 
+            return keys[page] as keyof typeof projectData
+        }
         
         // Change button based on if user already has role
-        let roleName = projects[page].fields[1].value
-        if (member.roles.cache.some((role: any) => role.name === roleName)) {
-            row = remove
-        }
-        else {
-            row = assign
+        const setRow = () => {
+            let roleName = projectData[getProject(page)].fields[1].value
+            if (member.roles.cache.some((role: any) => role.name === roleName)) {
+                row = remove
+            }
+            else {
+                row = assign
+            }
         }
 
-        await interaction.reply({embeds: [projects[page]], components: [row]})
+        setRow()
+
+        await interaction.reply({embeds: [projectData[getProject(page)]], components: [row]})
+        
 
         const filter = (button: MessageComponentInteraction) => button.customId === "backward" || button.customId === "forward" || button.customId === "assign-role" || button.customId === "remove-role";
         const collector = interaction.channel?.createMessageComponentCollector({filter})
@@ -89,37 +73,40 @@ export const projects: ICommand = {
                 if (page >= 1) {
                     page -= 1
                 }
-                await interaction.editReply({embeds: [projects[page]]})
+
+                setRow()
+                await interaction.editReply({embeds: [projectData[getProject(page)]], components: [row]})
                 button.deferUpdate()
             }
 
             if (button.customId === "forward") {
-                if (page <= projects.length - 2) {
+                if (page <= keys.length - 2) {
                     page += 1
                 }
 
-                await interaction.editReply({embeds: [projects[page]]})
+                setRow()
+                await interaction.editReply({embeds: [projectData[getProject(page)]], components: [row]})
                 button.deferUpdate()
             }
 
             if (button.customId === "assign-role") {
                 // Find Role and Add to member
-                let roleName = projects[page].fields[1].value
+                let roleName = projectData[getProject(page)].fields[1].value
                 var role = interaction.guild?.roles.cache.find(role => role.name === roleName)
                 member.roles.add(role)
                 row = remove
-                await interaction.editReply({embeds: [projects[page]], components: [row]})
+                await interaction.editReply({embeds: [projectData[getProject(page)]], components: [row]})
                 button.deferUpdate()
 
             }
 
             if (button.customId === "remove-role") {
                 // Find role and remove from member
-                let roleName = projects[page].fields[1].value
+                let roleName = projectData[getProject(page)].fields[1].value
                 var role = interaction.guild?.roles.cache.find(role => role.name === roleName)
                 member.roles.remove(role)
                 row = assign
-                await interaction.editReply({embeds: [projects[page]], components: [row]})
+                await interaction.editReply({embeds: [projectData[getProject(page)]], components: [row]})
                 button.deferUpdate()
             }
         })
