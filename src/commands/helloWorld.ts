f68edc1955b557d4dd72e57948c18a26f03bc237
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2022-04-27 12:01:44 -0400

    Hello world (#22)
    * project command
    
    * project command
    
    * add/remove role from project page
    
    * forgot to change role
    
    * embeds are editable through file
    
    * user can add resource to project
    
    * error handling when role undefined
    
    * documiner role typo
    
    * check officer and open roles
    
    * bug fix
    
    * officer check for add-resource
    
    * typo
    
    * typo
    
    * base structure
    
    * added some languages
    
    * command to list all languages
    
    * case insensitive/name changes
    
    Co-authored-by: Dev Patel <devpatel2556@gmail.com>

---
+++ src/commands/helloWorld.ts
@@ -0,0 +1,44 @@
+import { BaseCommandInteraction, Constants} from "discord.js";
+import { ICommand } from "../icommand";
+import helloWorldData from "../hello_worlds.json"
+
+const helloWorld: ICommand = {
+    name: "helloworld",
+    description: "Hello World in different languages",
+    options: [
+        {
+            name: "language",
+            description: "Programming language",
+            required: true,
+            type: Constants.ApplicationCommandOptionTypes.STRING
+        }
+    ],
+
+    execute: async (interaction: BaseCommandInteraction) => {
+        const language = (interaction.options.get("language")?.value as string).toLowerCase()
+        const key = language as keyof typeof helloWorldData
+        
+        if (helloWorldData[key] === undefined) {
+            await interaction.reply("Language not available")
+            return
+        }
+
+        await interaction.reply("```"+language+"\n"+helloWorldData[key]+"\n```")
+    }
+
+}
+
+const helloWorldLanguages: ICommand = {
+    name: "helloworld-list",
+    description: "List of Hello World Languages",
+    
+    execute: async (interaction: BaseCommandInteraction) => {
+        const keys = Object.keys(helloWorldData)
+        const languages = keys.join("\n")
+        await interaction.reply("```\n"+languages+"\n```")
+    }
+}
+
+export function getCommands(): ICommand[] {
+    return [helloWorld, helloWorldLanguages];
+}
