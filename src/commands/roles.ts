dc3eff848448dd5b5910f85dc37ed4f796aadf12
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2022-04-28 15:29:03 -0400

    remove hardcoding of officer (#23)
    * remove hardcoding of officer
    
    * formatting
    
    * formatting 2

--- src/commands/roles.ts
+++ src/commands/roles.ts
@@ -1,155 +1,153 @@
-import { Base, BaseCommandInteraction, Constants, Interaction} from "discord.js";
-import { ICommand } from "../icommand";
-import config from '../../config.json'
-import { dir } from "console";
-let roles = new Set<string>();
-
-export let permitted = new Set<string>();
-
-let refreshRoles = async function (interaction: BaseCommandInteraction): Promise<any> {
-    let guild: any = interaction.client.guilds.cache.get(config.guildId)
-    roles.clear()
-    const role = await guild.roles.cache.forEach((r: any) => roles.add(r.name))
-     roles.forEach((role: string) => {
-        if (role.startsWith('@')) {
-          roles.delete(role);
-        }
-      })
-
-      if(interaction.client.user != null)
-      { 
-           roles.delete(interaction.client.user.username)
+import {
+    Base,
+    BaseCommandInteraction,
+    Constants,
+    Interaction,
+  } from "discord.js";
+  import { ICommand } from "../icommand";
+  import config from "../../config.json";
+  import { dir } from "console";
+  let roles = new Set<string>();
+  
+  export let permitted = new Set<string>();
+  
+  let refreshRoles = async function (
+    interaction: BaseCommandInteraction
+  ): Promise<any> {
+    let guild: any = interaction.client.guilds.cache.get(config.guildId);
+    roles.clear();
+    const role = await guild.roles.cache.forEach((r: any) => roles.add(r.name));
+    roles.forEach((role: string) => {
+      if (role.startsWith("@")) {
+        roles.delete(role);
       }
-
-}
-const GiveRole: ICommand = {
+    });
+  
+    if (interaction.client.user != null) {
+      roles.delete(interaction.client.user.username);
+    }
+  };
+  const GiveRole: ICommand = {
     name: "giverole",
     description: "Gives a user a role",
     options: [
-        {
-            name: "role",
-            description: "Role that user wants",
-            required: true,
-            type: Constants.ApplicationCommandOptionTypes.STRING
-        }
+      {
+        name: "role",
+        description: "Role that user wants",
+        required: true,
+        type: Constants.ApplicationCommandOptionTypes.STRING,
+      },
     ],
     execute: async (interaction: BaseCommandInteraction) => {
-        await refreshRoles(interaction)
-        let name = interaction.options.get("role")?.value;
-        let guild: any = interaction.client.guilds.cache.get(config.guildId)
-        const member = await guild.members.fetch(interaction.user.id)
-
-        if((roles.has(name as string) && permitted.has(name as string)) || (member.permissions.has('MANAGE_ROLES') && roles.has(name as string)) )
-	    {
-            const role = await guild.roles.cache.find((r: any) => r.name === (name as string))
-            member.roles.add(role)
-            await interaction.reply(`Added to role successfully!`);
-
-        }
-        else
-        {
-            await interaction.reply(`No such role or not permitted`);
-
-        }
-    }
-}
-const OpenRole: ICommand = {
+      await refreshRoles(interaction);
+      let name = interaction.options.get("role")?.value;
+      let guild: any = interaction.client.guilds.cache.get(config.guildId);
+      const member = await guild.members.fetch(interaction.user.id);
+  
+      if (
+        (roles.has(name as string) && permitted.has(name as string)) ||
+        (member.permissions.has("ADMINISTRATOR") && roles.has(name as string))
+      ) {
+        const role = await guild.roles.cache.find(
+          (r: any) => r.name === (name as string)
+        );
+        member.roles.add(role);
+        await interaction.reply(`Added to role successfully!`);
+      } else {
+        await interaction.reply(`No such role or not permitted`);
+      }
+    },
+  };
+  const OpenRole: ICommand = {
     name: "openrole",
     description: "Creates a role",
     options: [
-        {
-            name: "role",
-            description: "Role that admin wants to give permissions for",
-            required: true,
-            type: Constants.ApplicationCommandOptionTypes.STRING
-        }
+      {
+        name: "role",
+        description: "Role that admin wants to give permissions for",
+        required: true,
+        type: Constants.ApplicationCommandOptionTypes.STRING,
+      },
     ],
     execute: async (interaction: BaseCommandInteraction) => {
-        await refreshRoles(interaction)
-
-        let name = interaction.options.get("role")?.value;
-        let guild: any = interaction.client.guilds.cache.get(config.guildId)
-        const member = await guild.members.fetch(interaction.user.id)
-        if((member.permissions.has('MANAGE_ROLES') && roles.has(name as string)))
-	    {            
-            if((name as string) !== "officer")
-           {
-                permitted.add((name as string))
-                await interaction.reply(`role permissions saved successfully!`);
-           }
-           else
-           {
-            await interaction.reply(`Officer channel access blocked`);
-
-           }
-
+      await refreshRoles(interaction);
+  
+      let name = interaction.options.get("role")?.value;
+      let guild: any = interaction.client.guilds.cache.get(config.guildId);
+      const member = await guild.members.fetch(interaction.user.id);
+      if (member.permissions.has("ADMINISTRATOR") && roles.has(name as string)) {
+        const role = await guild.roles.cache.find(
+          (r: any) => r.name === (name as string)
+        );
+  
+        if (!role.permissions.has("ADMINISTRATOR")) {
+          permitted.add(name as string);
+          await interaction.reply(`Role permissions saved successfully!`);
+        } else {
+          await interaction.reply(
+            `Cannot open role with administrator privileges`
+          );
         }
-        else 
-        {
-            await interaction.reply(`You are not an officer or something else went wrong`);
-
-        }
-    }
-}
-const CloseRole: ICommand = {
+      } else {
+        await interaction.reply(
+          `You are not an admin or something else went wrong`
+        );
+      }
+    },
+  };
+  const CloseRole: ICommand = {
     name: "closerole",
     description: "Closes a role",
     options: [
-        {
-            name: "role",
-            description: "Role that admin wants to close permissions for",
-            required: true,
-            type: Constants.ApplicationCommandOptionTypes.STRING
-        }
+      {
+        name: "role",
+        description: "Role that admin wants to close permissions for",
+        required: true,
+        type: Constants.ApplicationCommandOptionTypes.STRING,
+      },
     ],
     execute: async (interaction: BaseCommandInteraction) => {
-        await refreshRoles(interaction)
-
-        let name = interaction.options.get("role")?.value;
-        let guild: any = interaction.client.guilds.cache.get(config.guildId)
-        const member = await guild.members.fetch(interaction.user.id)
-
-        if((member.permissions.has('MANAGE_ROLES') && roles.has(name as string)))
-	    {             
- 
-            if(permitted.has(name as string))
-             {   
-                permitted.delete((name as string))
-                await interaction.reply(`role permissions removed successfully!`)
-             }
-             else
-                await interaction.reply(`either role does not exist or is already open!`);
-
-
-        }
-        else 
-        {
-            await interaction.reply(`You are not an officer or something else went wrong`);
-
-        }
-    }
-}
-const ShowRoles: ICommand = {
+      await refreshRoles(interaction);
+  
+      let name = interaction.options.get("role")?.value;
+      let guild: any = interaction.client.guilds.cache.get(config.guildId);
+      const member = await guild.members.fetch(interaction.user.id);
+  
+      if (member.permissions.has("ADMINISTRATOR") && roles.has(name as string)) {
+        if (permitted.has(name as string)) {
+          permitted.delete(name as string);
+          await interaction.reply(`role permissions removed successfully!`);
+        } else
+          await interaction.reply(
+            `either role does not exist or is already open!`
+          );
+      } else {
+        await interaction.reply(
+          `You are not an admin or something else went wrong`
+        );
+      }
+    },
+  };
+  const ShowRoles: ICommand = {
     name: "roles",
     description: "See roles",
-    
+  
     execute: async (interaction: BaseCommandInteraction) => {
-            await refreshRoles(interaction)
-
-            let divider: string = "----------------"
-            let messageLines: string[] = ["```", "Roles", divider];
-            roles.forEach(role => messageLines.push(
-               `${role}${permitted.has(role)? '*' : ''}`
-            ));
-            messageLines.push("*members can self-assign");
-
-            messageLines.push("```")
-            await interaction.reply(messageLines.join("\n"));
-            return;
-        
-    }
-
-}
-export function getCommands(): ICommand[] {
+      await refreshRoles(interaction);
+  
+      let divider: string = "----------------";
+      let messageLines: string[] = ["```", "Roles", divider];
+      roles.forEach((role) =>
+        messageLines.push(`${role}${permitted.has(role) ? "*" : ""}`)
+      );
+      messageLines.push("*members can self-assign");
+  
+      messageLines.push("```");
+      await interaction.reply(messageLines.join("\n"));
+      return;
+    },
+  };
+  export function getCommands(): ICommand[] {
     return [GiveRole, OpenRole, CloseRole, ShowRoles];
-}
+  }
+  
